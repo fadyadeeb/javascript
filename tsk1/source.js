668ad22ed7607692d983d067e83c0fa5e31c80f4
fady adeeb <fadyadeebanwer@gmail.com>
2020-11-18 00:19:34 +0200

    first commit

---
+++ tsk1/source.js
@@ -0,0 +1,14 @@
+var marks=49;
+
+
+if(marks>=85){
+    console.log("you are excellent");
+}else if(marks>=75){
+    console.log("you are very good");
+}else if(marks>=65){
+    console.log("you are good");
+}else if(marks>=50){
+    console.log("you are passed");
+}else {
+    console.log("you are faild");
+}
