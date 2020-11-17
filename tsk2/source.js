c63e96dc6ace76cbcaf851e66867ce85c03424b0
fady adeeb <fadyadeebanwer@gmail.com>
2020-11-18 01:05:13 +0200

    first commit

---
+++ tsk2/source.js
@@ -0,0 +1,17 @@
+var x=5;
+var y=6;
+
+var operator= "+";
+
+
+if(operator=="+"){
+    alert(x+y);
+}else if(operator=="-"){
+    alert(x-y);
+}else if(operator=="/"){
+    alert(x/y);
+}else if(operator=="*"){
+    alert(x*y);
+}else{
+    alert("please enter correct information");
+}
